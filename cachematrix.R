f6954b636827d68bd53a656bb269d7bc92830df1
Karthik Murthy <kmurthy@techpanther.com>
2015-05-24 11:02:29 -0600

    Committing cachematrix.R with completed function definitions

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,39 @@
 ## functions do
 
 ## Write a short comment describing this function
+## This function creates a special "matrix", which is really a list containing functions to 
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the value of the matrix's inverse
+## 4. get the value of the matrix's inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+      x <<- y
+      inv <<- NULL
+    }
+    get <- function() x
+    setInv <- function(inverse) inv <<- inverse
+    getInv <- function() inv
+    list(set = set, get = get,
+         setInv = setInv,
+         getInv = getInv)
 }
 
 
 ## Write a short comment describing this function
+## Gets the inverse for a matrix x (assuming that the inverse can be calculated) and determines whether it is null or not
+## If it is null, it calculates the inverse for x and then caches them for later use
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInv()
+  if(!is.null(inv)) {
+    return(inv)
+  }
+  mat <- x$get()
+  newInv <- solve(mat,...)
+  x$setInv(newInv)
+  newInv
 }
